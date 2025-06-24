991ba9db6af9e2c3ee452528738cc7606cf8887d
BixentEB <88851302+BixentEB@users.noreply.github.com>
2025-06-24 04:02:39 +0200

    Update atelier.js

--- assets/js/atelier.js
+++ assets/js/atelier.js
@@ -1,6 +1,20 @@
 // assets/js/atelier.js
+
+// Injection du menu
 fetch('/atelier/atelier-menu.html')
   .then(res => res.text())
   .then(html => {
     document.getElementById('atelier-menu').innerHTML = html;
   });
+
+// Injection du viewer dynamique
+window.chargerProjet = function(nom) {
+  fetch(`/atelier/${nom}.html`)
+    .then(response => response.text())
+    .then(html => {
+      document.getElementById("article-viewer").innerHTML = html;
+    })
+    .catch(error => {
+      document.getElementById("article-viewer").innerHTML = `<p class='erreur'>Erreur de chargement : ${nom}.html</p>`;
+    });
+};
