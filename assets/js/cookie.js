e358217b1dcbc767e82ce58c1532ab0c72d21ca0
BixentEB <88851302+BixentEB@users.noreply.github.com>
2025-07-04 13:46:46 +0200

    Create cookie.js

---
+++ assets/js/cookie.js
@@ -0,0 +1,8 @@
+if (!localStorage.getItem('cookieConsent')) {
+  document.getElementById('cookie-banner').style.display = 'flex';
+}
+
+document.getElementById('cookie-accept').addEventListener('click', function() {
+  localStorage.setItem('cookieConsent', 'true');
+  document.getElementById('cookie-banner').style.display = 'none';
+});
