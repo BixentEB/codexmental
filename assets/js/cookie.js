68d74375fb93a74fdfe429fa587fb45ad2e1700c
BixentEB <88851302+BixentEB@users.noreply.github.com>
2025-07-04 13:55:54 +0200

    Update cookie.js

--- assets/js/cookie.js
+++ assets/js/cookie.js
@@ -1,8 +1,10 @@
-if (!localStorage.getItem('cookieConsent')) {
-  document.getElementById('cookie-banner').style.display = 'flex';
-}
+window.addEventListener('DOMContentLoaded', function() {
+  if (!localStorage.getItem('cookieConsent')) {
+    document.getElementById('cookie-banner').style.display = 'flex';
+  }
 
-document.getElementById('cookie-accept').addEventListener('click', function() {
-  localStorage.setItem('cookieConsent', 'true');
-  document.getElementById('cookie-banner').style.display = 'none';
+  document.getElementById('cookie-accept').addEventListener('click', function() {
+    localStorage.setItem('cookieConsent', 'true');
+    document.getElementById('cookie-banner').style.display = 'none';
+  });
 });
