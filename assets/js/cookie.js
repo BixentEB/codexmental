0d82609efdc23bfbe2d5b55ba0da547b1bd89814
BixentEB <88851302+BixentEB@users.noreply.github.com>
2025-07-05 14:29:18 +0200

    Update cookie.js

--- assets/js/cookie.js
+++ assets/js/cookie.js
@@ -1,10 +1,21 @@
+function setCookie(name, value, days) {
+  const expires = new Date(Date.now() + days * 24 * 60 * 60 * 1000).toUTCString();
+  document.cookie = name + "=" + encodeURIComponent(value) + "; expires=" + expires + "; path=/";
+}
+
+function getCookie(name) {
+  const value = "; " + document.cookie;
+  const parts = value.split("; " + name + "=");
+  if (parts.length === 2) return decodeURIComponent(parts.pop().split(";").shift());
+}
+
 window.addEventListener('DOMContentLoaded', function() {
-  if (!localStorage.getItem('cookieConsent')) {
+  if (!getCookie('cookieConsent')) {
     document.getElementById('cookie-banner').style.display = 'flex';
   }
 
   document.getElementById('cookie-accept').addEventListener('click', function() {
-    localStorage.setItem('cookieConsent', 'true');
+    setCookie('cookieConsent', 'true', 365);
     document.getElementById('cookie-banner').style.display = 'none';
   });
 });
