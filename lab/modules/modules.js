27d64038dae3bded4055c0f9c7ece006ddf5f2b5
BixentEB <88851302+BixentEB@users.noreply.github.com>
2025-07-18 00:45:24 +0200

    Update modules.js

--- lab/modules/modules.js
+++ lab/modules/modules.js
@@ -2,6 +2,7 @@
 
 // Chargement manuel des modules à activer
 import "/lab/modules/simul-moon/simul-moon.js";
+import "/lab/modules/simul-moon/simul_moon_v_2.js";
 
 // D'autres imports à activer au besoin :
 // import "/lab/modules/widget-solaire/widget-solaire.js";
