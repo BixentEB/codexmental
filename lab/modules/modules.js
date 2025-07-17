cc4cd47fea1958a648c5e362b97062677aff9c78
BixentEB <88851302+BixentEB@users.noreply.github.com>
2025-07-17 20:53:47 +0200

    Create modules.js

---
+++ lab/modules/modules.js
@@ -0,0 +1,10 @@
+// modules.js – Chargement centralisé des widgets du Lab Codex
+
+// Chargement manuel des modules à activer
+import "/lab/modules/simul-moon/simul-moon.js";
+
+// D'autres imports à activer au besoin :
+// import "/lab/modules/widget-solaire/widget-solaire.js";
+// import "/lab/modules/widget-astro/widget-astro.js";
+
+console.log("✅ Modules du lab chargés");
