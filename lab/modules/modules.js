a1a3499e013cbb6722a49ea497a30916b6af78b5
BixentEB <88851302+BixentEB@users.noreply.github.com>
2025-07-18 18:21:50 +0200

    Update modules.js

--- lab/modules/modules.js
+++ lab/modules/modules.js
@@ -3,16 +3,19 @@
 // Chargement manuel des modules à activer
 import { launchSimulMoon } from "/lab/modules/simul-moon/simul-moon.js";
 
-// D'autres imports à activer au besoin :
-// import "/lab/modules/widget-solaire/widget-solaire.js";
-// import "/lab/modules/widget-astro/widget-astro.js";
-
 // Ne lancer le simulateur que sur /lab/index.html
 if (window.location.pathname.endsWith("/lab/index.html")) {
   launchSimulMoon();
 }
 
 console.log("✅ Modules du lab chargés");
+
+
+// D'autres imports à activer au besoin :
+// import "/lab/modules/widget-solaire/widget-solaire.js";
+// import "/lab/modules/widget-astro/widget-astro.js";
+
+
 
 
 
