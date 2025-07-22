a6b0092d18e96cc7cc39b93ce842ce30fde825ad
BixentEB <88851302+BixentEB@users.noreply.github.com>
2025-07-22 15:50:45 +0200

    Update modules.js

--- lab/modules/modules.js
+++ lab/modules/modules.js
@@ -6,13 +6,15 @@ if (location.pathname === '/lab/index.html') {
 }
 
 if (window.location.pathname.endsWith("/lab/index.html")) {
-  import("/lab/modules/dashboard/planet-data.js");
-  import("/lab/modules/dashboard/planet-database.js"); // ✅ Ajout requis ici
-  import("/lab/modules/dashboard/radar-mini.js");
+  import("/lab/modules/dashboard/planet-database.js");     // 🧠 Données planètes
+  import("/lab/modules/dashboard/moon-database.js");       // 🌙 Données lunes
+  import("/lab/modules/dashboard/planet-data.js");         // 🧩 Injection UI
+  import("/lab/modules/dashboard/display-moons.js");       // 🌘 Affichage lune
+  import("/lab/modules/dashboard/viewer-planete-3d.js");   // 🔭 Canvas 3D
+  import("/lab/modules/dashboard/simul-system.js");        // ☀️ Radar solaire
+  import("/lab/modules/dashboard/radar-mini.js");          // 🎯 Radar décoratif
   import("/lab/modules/simul-moon/simul-moon-canvas.js")
     .then(mod => mod.launchSimulMoonCanvas());
-  import("/lab/modules/dashboard/viewer-planete-3d.js");
-  import("/lab/modules/dashboard/simul-system.js");
 }
 
 console.log("✅ Modules du lab chargés");
