7a757ba599c1143925babac1ec1995efc12302ce
BixentEB <88851302+BixentEB@users.noreply.github.com>
2025-08-07 07:14:13 +0200

    Update openmenu.js

--- assets/js/openmenu.js
+++ assets/js/openmenu.js
@@ -1,19 +1,12 @@
-// ========================================================
-// openmenu.js - 
-// gérer le comportement exclusif d’ouverture des menus <details>
-// Se déclenche à chaque ouverture - Ferme tous les autres <details> sauf celui qu'on vient d’ouvrir
-// ========================================================
-
-<script>
-  document.querySelectorAll('details').forEach((menu) => {
-    menu.addEventListener('toggle', () => {
-      if (menu.open) {
-        document.querySelectorAll('details').forEach((other) => {
-          if (other !== menu) {
-            other.removeAttribute('open');
-          }
-        });
-      }
-    });
+// openmenu.js – Accordéon exclusif pour les menus <details> - ferme auto les menus ouverts quand on ouvre un autre
+document.querySelectorAll('details').forEach((menu) => {
+  menu.addEventListener('toggle', () => {
+    if (menu.open) {
+      document.querySelectorAll('details').forEach((other) => {
+        if (other !== menu) {
+          other.removeAttribute('open');
+        }
+      });
+    }
   });
-</script>
+});
